2f74d3686dcc23e07281e2fe5c81166e70e7f022
Justas Masiulis <Justas.masiulis@yahoo.com>
2017-09-09 16:25:06 +0300

    added error.hpp for windows

--- include/detail/windows/error.hpp
+++ include/detail/windows/error.hpp
@@ -1,3 +1,22 @@
+#ifndef VMU_WINDOWS_ERROR_HPP
+#define VMU_WINDOWS_ERROR_HPP
 
-namespace vmu{ namespace detail {
+#include <system_error>
+#include "winapi.hpp"
+
+namespace vmu { namespace detail {
+
+    inline std::error_code get_last_error() noexcept
+    {
+        return std::error_code(static_cast<int>(winapi::GetLastError())
+                               , std::system_category());
+    }
+
+    inline void throw_last_error(const char* msg)
+    {
+        throw std::system_error(get_last_error(), msg);
+    }
+
 }}
+
+#endif // !VMU_WINDOWS_ERROR_HPP
