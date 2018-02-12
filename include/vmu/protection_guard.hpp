160ee050c905443b2368c382d7bff0bfd11fd8ea
Justas Masiulis <justas.masiulis@yahoo.com>
2018-02-12 20:58:43 +0200

    fixed a protection_guard bug that may have been causing all the issues

--- include/vmu/protection_guard.hpp
+++ include/vmu/protection_guard.hpp
@@ -1,18 +1,18 @@
 /*
-* Copyright 2017 Justas Masiulis
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2017 Justas Masiulis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef VMU_PROTECTION_GUARD_HPP
 #define VMU_PROTECTION_GUARD_HPP
@@ -23,7 +23,9 @@
 
 namespace vmu {
 
-    struct adopt_protection_t { explicit adopt_protection_t() = default; };
+    struct adopt_protection_t {
+        explicit adopt_protection_t() = default;
+    };
 
     /// \brief Provides a RAII based page protection
     class protection_guard {
@@ -34,13 +36,14 @@ namespace vmu {
         };
 
         std::vector<old_prot_storage> _old;
-    public:
 
-        /// \brief Restores the memory protection to its state before the protection_guard constructor was called.
-        /// \throws Does not throw.
-        /// \note May not actually restore memory protection back to its original state.
-        ///       In critical code sections prefer to manually call restore member function.
-        ///       This behaviour may be overriden by defining a VMU_HANDLE_PG_DESTRUCTOR_FAILURE macro.
+    public:
+        /// \brief Restores the memory protection to its state before the
+        /// protection_guard constructor was called. \throws Does not throw. \note
+        /// May not actually restore memory protection back to its original state.
+        ///       In critical code sections prefer to manually call restore member
+        ///       function. This behaviour may be overriden by defining a
+        ///       VMU_HANDLE_PG_DESTRUCTOR_FAILURE macro.
         ~protection_guard()
         {
             std::error_code ec;
@@ -55,70 +58,85 @@ namespace vmu {
         }
 
         /// \brief Changes protection of a single page to given protection.
-        /// \param address The address that is situated in page whose protection is to be changed.
-        /// \param new_protection The new protection to be applied to the page.
+        /// \param address The address that is situated in page whose protection is
+        /// to be changed. \param new_protection The new protection to be applied to
+        /// the page.
         template<class Address>
         protection_guard(Address address, protection_t new_protection)
         {
-            const auto fixed_address = detail::uintptr_cast(address) & ~(page_size() - 1);
+            const auto fixed_address =
+                detail::uintptr_cast(address) & ~(page_size() - 1);
             const auto old = query<std::uintptr_t>(address);
             _old.reserve(1);
-            _old.push_back({fixed_address, fixed_address + 1, old.protection().native()});
+            _old.push_back(
+                { fixed_address, fixed_address + 1, old.protection().native() });
             protect(address, new_protection);
         }
 
-        /// \brief Changes protection of pages between range [begin; end) to given protection.
-        /// \param begin The beginning of the memory range to apply new protection to.
-        /// \param end One pas the end of the memory range to apply new protection to.
-        /// \param new_protection The new protection to be applied to the pages.
+        /// \brief Changes protection of pages between range [begin; end) to given
+        /// protection. \param begin The beginning of the memory range to apply new
+        /// protection to. \param end One pas the end of the memory range to apply
+        /// new protection to. \param new_protection The new protection to be applied
+        /// to the pages.
         template<class Address>
         protection_guard(Address begin, Address end, protection_t new_protection)
         {
             const auto regions = query_range<std::uintptr_t>(begin, end);
             _old.reserve(regions.size());
-            for (auto& region : regions)
-                if (region)
-                    _old.push_back({region.begin(), region.end(), region.protection().native()});
+            for (auto& region : regions) {
+                _old.push_back(
+                    { std::max(region.begin(), detail::uintptr_cast(begin)),
+                      std::min(region.end(), detail::uintptr_cast(end)),
+                      region.protection().native() });
+            }
 
             protect(begin, end, new_protection);
         }
 
-        /// \brief Changes protection of pages that the given range crosses to given protection.
-        /// \param range The memory range whose protection is to be changed.
-        /// \param new_protection The new protection to be applied to the pages.
-        template<class Range, class = typename std::enable_if<!is_address<Range>::value>::type>
+        /// \brief Changes protection of pages that the given range crosses to given
+        /// protection. \param range The memory range whose protection is to be
+        /// changed. \param new_protection The new protection to be applied to the
+        /// pages.
+        template<class Range,
+                 class = typename std::enable_if<!is_address<Range>::value>::type>
         protection_guard(const Range& range, protection_t new_protection)
-                : protection_guard(range.begin(), range.end(), new_protection) {}
+            : protection_guard(range.begin(), range.end(), new_protection)
+        {}
 
-        /// \brief Changes protection of a single page to given protection when the protection_guard object is destructed.
-        /// \param range The memory range whose protection is to be changed.
-        /// \param new_protection The new protection to be applied to the pages.
+        /// \brief Changes protection of a single page to given protection when the
+        /// protection_guard object is destructed. \param range The memory range
+        /// whose protection is to be changed. \param new_protection The new
+        /// protection to be applied to the pages.
         template<class Address>
-        protection_guard(Address address, protection_t restore_to, adopt_protection_t)
+        protection_guard(Address      address,
+                         protection_t restore_to,
+                         adopt_protection_t)
         {
             const auto fixed_address = detail::uintptr_cast(address) & -page_size();
             _old.reserve(1);
             _old.push_back({ fixed_address, fixed_address + 1, restore_to });
         }
 
-        /// \brief Changes the protection of pages in memory range [begin; end) upon destruction of protection_guard object
-        /// \param range The memory range whose protection is to be changed.
-        /// \param new_protection The new protection to be applied to the pages.
+        /// \brief Changes the protection of pages in memory range [begin; end) upon
+        /// destruction of protection_guard object \param range The memory range
+        /// whose protection is to be changed. \param new_protection The new
+        /// protection to be applied to the pages.
         template<class Address>
-        protection_guard(Address begin
-                         , Address end
-                         , protection_t restore_to
-                         , adopt_protection_t)
+        protection_guard(Address      begin,
+                         Address      end,
+                         protection_t restore_to,
+                         adopt_protection_t)
         {
             _old.reserve(1);
-            _old.emplace_back(detail::address_cast_unchecked<std::uintptr_t>(begin)
-                              , detail::address_cast<std::uintptr_t>(end)
-                              , restore_to);
+            _old.emplace_back(detail::address_cast_unchecked<std::uintptr_t>(begin),
+                              detail::address_cast<std::uintptr_t>(end),
+                              restore_to);
         }
 
         template<class Range, class Address>
         protection_guard(const Range& r, protection_t prot, adopt_protection_t)
-                : protection_guard(r.begin(), r.end(), prot, adopt_protection_t{}) {}
+            : protection_guard(r.begin(), r.end(), prot, adopt_protection_t{})
+        {}
 
 
         /// \brief not copy constructible
@@ -148,7 +166,8 @@ namespace vmu {
         }
 
         /// \brief Releases protections to restore.
-        /// \throws Should not throw due to the fact that shrink_to_fit is called on empty vector.
+        /// \throws Should not throw due to the fact that shrink_to_fit is called on
+        /// empty vector.
         void release()
         {
             _old.clear();
@@ -156,6 +175,6 @@ namespace vmu {
         }
     };
 
-}
+} // namespace vmu
 
 #endif // include guard
--- test/main.cpp
+++ test/main.cpp
@@ -10,54 +10,56 @@ void check_region(const vmu::basic_region<Address> region, Address2 addr)
     CHECK(region.protection().readable());
     CHECK(region.protection().writable());
     CHECK(region.size() != 0);
-    CHECK(vmu::detail::uintptr_cast(region.begin()) <= vmu::detail::uintptr_cast(addr));
-    CHECK(vmu::detail::uintptr_cast(region.end()) >= vmu::detail::uintptr_cast(addr));
+    CHECK(vmu::detail::uintptr_cast(region.begin()) <=
+          vmu::detail::uintptr_cast(addr));
+    CHECK(vmu::detail::uintptr_cast(region.end()) >=
+          vmu::detail::uintptr_cast(addr));
 }
 
 TEST_CASE("address_cast sanity check")
 {
     std::uintptr_t uintptr = 0xdeadbeef;
-    void* ptr = vmu::detail::address_cast<void*>(uintptr);
+    void*          ptr     = vmu::detail::address_cast<void*>(uintptr);
     REQUIRE(memcmp(&uintptr, &ptr, sizeof(void*)) == 0);
 }
 
 TEST_CASE("query")
 {
-    int  i     = 5;
-    auto ret   = vmu::query(&i);
+    int  i   = 5;
+    auto ret = vmu::query(&i);
     check_region(ret, &i);
 }
 
 TEST_CASE("query error code")
 {
     std::error_code ec;
-    int  i     = 5;
-    auto ptr_i = reinterpret_cast<std::uintptr_t>(&i);
-    auto ret   = vmu::query(ptr_i, ec);
+    int             i     = 5;
+    auto            ptr_i = reinterpret_cast<std::uintptr_t>(&i);
+    auto            ret   = vmu::query(ptr_i, ec);
     check_region(ret, ptr_i);
 }
 
 TEST_CASE("query_range")
 {
-    int  i    = 5;
-    auto ptr  = reinterpret_cast<std::uintptr_t>(&i);
+    int        i    = 5;
+    auto       ptr  = reinterpret_cast<std::uintptr_t>(&i);
     const auto rets = vmu::query_range(ptr, ptr + 16);
 
     REQUIRE_FALSE(rets.empty());
-    for(const auto& ret : rets)
+    for (const auto& ret : rets)
         check_region(ret, ptr);
 }
 
 TEST_CASE("query_range error code")
 {
-    int  i    = 5;
-    auto ptr  = reinterpret_cast<std::uintptr_t>(&i);
+    int             i   = 5;
+    auto            ptr = reinterpret_cast<std::uintptr_t>(&i);
     std::error_code ec;
-    const auto rets = vmu::query_range(&i, &i + 16, ec);
+    const auto      rets = vmu::query_range(&i, &i + 16, ec);
 
     REQUIRE_FALSE(ec);
     REQUIRE_FALSE(rets.empty());
-    for(const auto& ret : rets)
+    for (const auto& ret : rets)
         check_region(ret, ptr);
 }
 
@@ -89,36 +91,36 @@ TEST_CASE("protection_guard(Address, protection_t)")
 
 TEST_CASE("protection_guard(Address, Address, protection_t)")
 {
-    auto random_data = std::make_unique<unsigned char[]>(4096 * 3);
+    auto random_data = std::make_unique<unsigned char[]>(4096 * 6);
     auto testing_arr = random_data.get();
-    memset(testing_arr, 12, 4096 * 3);
+    memset(testing_arr, 0xCC, 4096 * 6);
     auto result = vmu::query(testing_arr);
     REQUIRE(result);
-
     {
         const auto new_prot = vmu::access::read;
         CHECK_FALSE(new_prot == result.protection().to_flags());
 
-        vmu::protection_guard pg(testing_arr, testing_arr + 4096 * 3, new_prot);
+        vmu::protection_guard pg(
+            testing_arr + 4097, testing_arr + 4096 * 3, new_prot);
 
-        auto new_flags = vmu::query(testing_arr);
+        auto new_flags = vmu::query(testing_arr + 4097);
         CHECK(new_flags);
         CHECK(new_flags.protection().to_flags() == new_prot);
     }
 
     auto result2 = vmu::query(testing_arr);
-    REQUIRE(result2.shared()                == result.shared());
-    REQUIRE(result2.begin()                 == result.begin());
+    REQUIRE(result2.shared() == result.shared());
+    REQUIRE(result2.begin() == result.begin());
     REQUIRE(result2.protection().to_flags() == result.protection().to_flags());
-    REQUIRE(result2.guarded()               == result.guarded());
+    REQUIRE(result2.guarded() == result.guarded());
 }
 
 TEST_CASE("query_iterator")
 {
-    const auto total_size = vmu::page_size() * 5;
-    auto random_data = std::make_unique<unsigned char[]>(total_size);
-    auto begin = random_data.get();
-    auto end = begin + total_size;
+    const auto total_size  = vmu::page_size() * 5;
+    auto       random_data = std::make_unique<unsigned char[]>(total_size);
+    auto       begin       = random_data.get();
+    auto       end         = begin + total_size;
 
     vmu::query_iterator end_it(end);
     for (vmu::query_iterator it(begin); it != end_it; ++it) {
